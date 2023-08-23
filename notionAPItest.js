53d8bb71f68ca7a6a3574898b09b6b2fb20b677f
Danuk <danuk77@live.co.uk>
2023-08-23 22:01:04 +0100

    Got the api integration working

--- notionAPItest.js
+++ notionAPItest.js
@@ -1,1 +1,45 @@
-console.log("hello world");
+const { Client } = require('@notionhq/client');
+
+// Authorise the program using the API key
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+const testNotion = async () => {
+    try{
+        const blockId = '04fb5ffc2fd142e290669caacdcd4e13';
+        const response = await notion.blocks.children.append({
+            block_id: blockId,
+            children: [
+            {
+                "heading_2": {
+                "rich_text": [
+                    {
+                    "text": {
+                        "content": "Danuk Sanujaya Udagama"
+                    }
+                    }
+                ]
+                }
+            },
+            {
+                "paragraph": {
+                "rich_text": [
+                    {
+                    "text": {
+                        "content": "This is actually connected to the page",
+                        "link": {
+                        "url": "https://www.youtube.com/watch?v=tdCN5ZP8Kfs"
+                        }
+                    }
+                    }
+                ]
+                }
+            }
+            ],
+        })
+        console.log(response);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+testNotion();
