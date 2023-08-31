a364a4241e28df259516bf7ed15408cafc93f5f3
Danuk <danuk77@live.co.uk>
2023-08-31 20:17:50 +0100

    Integrated backend functionalities with the UI

--- createNewEntry.js
+++ createNewEntry.js
@@ -1,24 +1,46 @@
 // Check the chrome storage to see if there is a configuration already set up for the extension
+const submit = document.getElementById("Submit");
+const settings = document.getElementById("Settings");
+const url = document.getElementById("URLin");
+const title = document.getElementById("Titlein");
+const description = document.getElementById("Descriptionin");
 
+// Values relating to the website the user is on
+let urlValue = null;
+let titleValue = null;
+let icon = null;
 
-document.getElementById("Send").addEventListener("click", async () => {
-    try{
+// Update the variables depending on what page the user is in
+try{
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, async tabs => {
+        urlValue = tabs[0].url;
+        titleValue = tabs[0].title;
+        icon = tabs[0].favIconUrl;
+
+        // Update the UI fields
+        url.value = tabs[0].url;
+        title.value = tabs[0].title;
+    });
+}catch(err){
+    console.log(err);
+}
+
+submit.addEventListener("click", async () => {
+    try{        
+        // Retrieve the information and make a call to the backend
         chrome.tabs.query({active: true, lastFocusedWindow: true}, async tabs => {
-            let url = tabs[0].url;
-            let title = tabs[0].title;
-            let iconURL = tabs[0].favIconUrl;
             let databaseID = 'fb62e501905e400c988b713255375537';
             let pageID = '04fb5ffc2fd142e290669caacdcd4e13';
-
             // Test version (The API endpoint for testing)
             const res = await fetch('http://127.0.0.1:5001/savetonotion-6acf8/us-central1/addDBEntry', {
                 method: 'POST',
                 body: JSON.stringify({
-                    url: url,
-                    title: title,
-                    icon: iconURL,
+                    url: urlValue,
+                    title: titleValue,
+                    icon: icon,
                     pageID: pageID,
-                    databaseID: databaseID
+                    databaseID: databaseID,
+                    description:description.value
                 })
             });
             // Production version (The API endpoint for production)
@@ -29,10 +51,30 @@ document.getElementById("Send").addEventListener("click", async () => {
             //     })
             // });
 
-            console.log(res);
+        console.log(res);
         });
 
     }catch(err){
         console.log(err);
     }
+});
+
+
+// Animations for clicking on buttons
+// Submit button
+submit.addEventListener("click", function() {
+    this.classList.add("clicked");
+
+    setTimeout(() => {
+        this.classList.remove("clicked");
+    }, 50);
+})
+
+// Settings button
+settings.addEventListener("click", function() {
+    this.classList.add("clicked");
+
+    setTimeout(() => {
+        this.classList.remove("clicked");
+    }, 50);
 })
